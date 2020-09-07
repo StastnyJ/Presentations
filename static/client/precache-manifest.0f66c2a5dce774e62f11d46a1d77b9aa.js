c4d65cf3a54019181b35bd707df3ff1ccb4814a3
StastnyJ <xstastnyj@gmail.com>
2020-09-07 02:42:34 +0200

    basic message

---
+++ static/client/precache-manifest.0f66c2a5dce774e62f11d46a1d77b9aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0a9f47f23f515c9509ed827fb211e2",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "4957fe1a4371a296c8b6",
+    "url": "/client/static/css/main.f5cc8eaa.chunk.css"
+  },
+  {
+    "revision": "c55bd04f56ba88d88c50",
+    "url": "/client/static/js/2.815761f4.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/client/static/js/2.815761f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4957fe1a4371a296c8b6",
+    "url": "/client/static/js/main.150719e7.chunk.js"
+  },
+  {
+    "revision": "a1c5e05baa254b3f31e7",
+    "url": "/client/static/js/runtime-main.37617a5f.js"
+  }
+]);
---
+++ static/client/precache-manifest.4858ed73cf10cedb9b67f0b7e152d4dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8553fd2b8344b4e069d51cd25c8f5fd7",
+    "url": "/static/client/index.html"
+  },
+  {
+    "revision": "4957fe1a4371a296c8b6",
+    "url": "/static/client/static/css/main.f5cc8eaa.chunk.css"
+  },
+  {
+    "revision": "cde5fdc314d8f0fab33b",
+    "url": "/static/client/static/js/2.3a468bf9.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/client/static/js/2.3a468bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4957fe1a4371a296c8b6",
+    "url": "/static/client/static/js/main.150719e7.chunk.js"
+  },
+  {
+    "revision": "bc330069849abcf36240",
+    "url": "/static/client/static/js/runtime-main.b5b9e709.js"
+  }
+]);
